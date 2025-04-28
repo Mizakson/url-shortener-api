af3b38cacbd2092a028bcf746776cf227d258bff
Mizakson <mizakson.dev@gmail.com>
2025-04-28 11:46:55 -0400

    Create generateShortCode method in index.js

---
+++ utils/index.js
@@ -0,0 +1,12 @@
+const crytpo = require("node:crypto")
+
+// generate shortCode with a default length of 8
+// using 'sha256' hashing algorithm
+function generateShortCode(url, length = 8) {
+    const hash = crytpo.createHash('sha256').update(url).digest('base64url')
+    const shortCode = hash.substring(0, length)
+
+    return shortCode
+}
+
+module.exports = generateShortCode
