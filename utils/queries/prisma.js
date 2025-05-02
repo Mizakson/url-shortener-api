64038309ceda29263c99dd59511ee63f36cd2aa4
Mizakson <mizakson.dev@gmail.com>
2025-05-02 13:39:08 -0400

    Update prisma, @prisma/client

--- utils/queries/prisma.js
+++ utils/queries/prisma.js
@@ -3,3 +3,15 @@ const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 
 module.exports = prisma
+
+// async function connectAndLog() {
+//     try {
+//         await prisma.$connect()
+//     } catch (error) {
+//         console.error("Error connecting to prisma: ", error)
+//     } finally {
+//         await prisma.$disconnect()
+//     }
+// }
+
+// connectAndLog()
