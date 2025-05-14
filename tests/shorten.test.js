9670910b58115b65f03e468fb4c9a8301f791a4c
Mizakson <mizakson.dev@gmail.com>
2025-05-14 11:48:36 -0400

    Refactor shorten tests with prisma methods

--- tests/shorten.test.js
+++ tests/shorten.test.js
@@ -1,62 +1,64 @@
 const request = require("supertest")
 const app = require("../app")
+const prisma = require("../utils/queries/prisma")
+const queries = require("../utils/queries")
 
-const wikipediaSearchUrl = `
-https://www.google.com/search?q=wikipedia&oq=wiki&gs_lcrp=
-EgZjaHJvbWUqCggAEAAYsQMYgAQyCggAEAAYsQMYgAQyDwgBEEUYORiDAR
-ixAxiABDIQCAIQLhjHARixAxjRAxiABDIGCAMQBRhAMgYIBBBFGEEyBggF
-EEUYQTIGCAYQRRhBMgYIBxBFGDzSAQgxMzA5ajBqN6gCCLACAfEFqHrKKM
--kGh8&sourceid=chrome&ie=UTF-8
-`
+let server
+const wikipediaSearchUrl = `https://www.google.com/search?q=wikipedia&oq=wiki&gs_lcrp=EgZjaHJvbWUqCggAEAAYsQMYgAQyCggAEAAYsQMYgAQyDwgBEEUYORiDARixAxiABDIQCAIQLhjHARixAxjRAxiABDIGCAMQBRhAMgYIBBBFGEEyBggFEEUYQTIGCAYQRRhBMgYIBxBFGDzSAQgxMzA5ajBqN6gCCLACAfEFqHrKKM-kGh8&sourceid=chrome&ie=UTF-8`
+const wikipediaSearchShortCode = "bAyjhsWZ"
 
 describe("POST /shorten", () => {
-    let server
     beforeAll(async () => {
         server = app.listen()
+        await prisma.$connect()
     })
 
-    it('recieves data and creates a new resource', (done) => {
+    // beforeEach(async () => {
+
+    // })
+
+    afterEach(async () => {
+        await prisma.url.deleteMany()
+    })
+
+    afterAll(async () => {
+        await prisma.$disconnect()
+        await server.close()
+    })
+
+    it('recieves data and creates a new resource', async () => {
+        const data = await queries.addUrl(wikipediaSearchUrl, wikipediaSearchShortCode)
         const response = request(server)
             .post("/api/shorten")
-            .send({ longUrl: wikipediaSearchUrl })
+            .send(data)
             .set("Content-Type", "application/json")
             .set("Accept", "application/json")
             .expect(201)
-            .end((err, res) => {
-                if (err) return done(err)
-                return done()
-            })
+
     })
 
-    it('should return 400 status if no url entered', (done) => {
+    it('should return 400 status if no url entered', async () => {
+        const data = queries.addUrl("", wikipediaSearchShortCode)
+
         const response = request(server)
             .post("/api/shorten")
-            .send({})
+            .send(data)
             .expect(400, {
                 error: "Please provide a url"
             })
-            .end((err, res) => {
-                if (err) return done(err)
-                return done()
-            })
     })
 
-    it('should return 400 status if invalid url entered', (done) => {
+    it('should return 400 status if invalid url entered', async () => {
+        const data = queries.addUrl(wikipediaSearchUrl, "")
+
         const response = request(server)
             .post("/api/shorten")
-            .send({ longUrl: "This is not a url" })
+            .send(data)
             .expect(400, {
                 error: "Please provide a valid url"
             })
-            .end((err, res) => {
-                if (err) return done(err)
-                return done()
-            })
+
     })
 
-
-    afterAll(async () => {
-        await server.close()
-    })
 })
 
