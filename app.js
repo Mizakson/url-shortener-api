015e0ffb5553b28c12983a8ad7aac50f940e4588
Mizakson <mizakson.dev@gmail.com>
2025-05-19 10:22:35 -0400

    Set initial rate limit for api (240 req/hr)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
+const { rateLimit } = require("express-rate-limit")
 
 const app = express()
 
@@ -8,12 +9,22 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const limiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1hr
+    limit: 240, // 4 requests per min 
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+    message: "request limit reached",
+})
+
+app.use(limiter)
+
 const apiRouter = require("./routes/index")
 
 app.use("/api", apiRouter)
 
 app.listen(3000, () => {
-    // console.log(`listening on ${process.env.BASE_URL}`)
+    console.log(`listening on ${process.env.BASE_URL}`)
     // comment out console statement when running tests
 })
 
