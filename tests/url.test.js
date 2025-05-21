fead9792c70d7b3bd7e9aa4c4a1d3a1f6324325a
Mizakson <mizakson.dev@gmail.com>
2025-05-21 11:49:51 -0400

    Refactor url tests

--- tests/url.test.js
+++ tests/url.test.js
@@ -9,8 +9,6 @@ let server
 const wikipediaSearchUrl = `https://www.google.com/search?q=wikipedia&oq=wiki&gs_lcrp=EgZjaHJvbWUqCggAEAAYsQMYgAQyCggAEAAYsQMYgAQyDwgBEEUYORiDARixAxiABDIQCAIQLhjHARixAxjRAxiABDIGCAMQBRhAMgYIBBBFGEEyBggFEEUYQTIGCAYQRRhBMgYIBxBFGDzSAQgxMzA5ajBqN6gCCLACAfEFqHrKKM-kGh8&sourceid=chrome&ie=UTF-8
 `
 
-const wikipediaSearchShortCode = "bAyjhsWZ"
-
 describe("GET /:shortCode", () => {
     beforeAll(async () => {
         server = app.listen()
@@ -31,34 +29,30 @@ describe("GET /:shortCode", () => {
     })
 
     it("should redirect to the original URL and increment timesClicked", async () => {
-        const data = await prisma.url.create({
-            data: {
-                originalUrl: wikipediaSearchUrl,
-                shortCode: wikipediaSearchShortCode
-            }
-        })
+        const entry = await queries.addUrl(wikipediaSearchUrl)
+
         const response = await request(server)
-            .get(`/api/${data.shortCode}`)
+            .get(`/api/${entry.data.shortCode}`)
             .expect(302)
 
         // the original header has %0A at the start and end of url
         const locationHeader = response.headers.location
         // console.log(locationHeader)
         const cleanedLocationHeader = locationHeader.replace(/%0A/g, "")
-        console.log(cleanedLocationHeader)
+        // console.log(cleanedLocationHeader)
         // trim to get actual strings to compare
-        expect(cleanedLocationHeader.trim()).toBe(data.originalUrl.trim())
+        expect(cleanedLocationHeader.trim()).toBe(entry.data.originalUrl.trim())
 
         // test to confirm increment counter works
-        const updatedCounter = await queries.incrementUrlTimesClicked(data.shortCode)
+        const updatedCounter = await queries.incrementUrlTimesClicked(entry.data.shortCode)
         expect(updatedCounter.timesClicked).toBe(1)
     })
+})
 
-    it("should return 404 if invalid shortCode is entered", async () => {
-        const fakeCode = "NotValidCode"
+it("should return 404 if invalid shortCode is entered", async () => {
+    const fakeCode = "NotValidCode"
 
-        const response = await request(server)
-            .get(`/api/${fakeCode}`)
-            .expect(404, { error: "Shortcode not found" })
-    })
+    const response = await request(server)
+        .get(`/api/${fakeCode}`)
+        .expect(404, { error: "Shortcode not found" })
 })
